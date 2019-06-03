15c353c4dfbd5f66cc6860a8b1774efb8f703122
kiotaku <takumi.kioka@gmail.com>
2019-06-03 14:20:01 +0900

    Froget 'from Kioka'

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send('Hello World from Kioka')
 })
 
 app.get('/test', (req, res) => {
