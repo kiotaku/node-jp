22d2bad56faebabcd9876769908fa5d574c0a273
kiotaku <takumi.kioka@gmail.com>
2019-06-10 14:27:07 +0900

    mongoose database connection, static html form

--- index.js
+++ index.js
@@ -3,7 +3,46 @@
 require('dotenv').config()
 
 const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+mongoose.Promise = Promise
 const app = express()
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+
+const userSchema = new Schema({
+  firstname: String,
+  lastname: String,
+  dateOfBirth: Date
+})
+
+const user = mongoose.model('User', userSchema)
+
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true }).then(() => {
+  console.log('Connected successfully.')
+  app.listen(process.env.PORT)
+}).catch(err => {
+  console.log('Connection to db failed: ' + err)
+})
+
+app.get('/user', (req, res) => {
+  user.find().then(users => {
+    res.send(users)
+  })
+})
+
+app.post('/user', (req, res) => {
+  console.log('data from http post', req.body)
+  user.create({
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    dateOfBirth: new Date(req.body.dob).getTime()
+  }).then(user => {
+    res.send(`user ${user.firstname} created with id: ${user._id}`)
+  })
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World from Kioka')
@@ -12,5 +51,3 @@ app.get('/', (req, res) => {
 app.get('/test', (req, res) => {
   res.send('Testing is fun')
 })
-
-app.listen(process.env.PORT)
