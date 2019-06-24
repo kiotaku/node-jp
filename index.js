9771df40c653a04399916a56db2733417b3cd929
kiotaku <takumi.kioka@gmail.com>
2019-06-24 14:17:51 +0900

    user routes in separated module

--- index.js
+++ index.js
@@ -10,26 +10,9 @@ const app = express()
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: false }))
 
-const user = require('./user/model')
-
 db.on('connected', () => app.listen(process.env.PORT))
 
-app.get('/user', (req, res) => {
-  user.find().then(users => {
-    res.send(users)
-  })
-})
-
-app.post('/user', (req, res) => {
-  console.log('data from http post', req.body)
-  user.create({
-    firstname: req.body.firstname,
-    lastname: req.body.lastname,
-    dateOfBirth: new Date(req.body.dob).getTime()
-  }).then(user => {
-    res.send(`user ${user.firstname} created with id: ${user._id}`)
-  })
-})
+app.use('/user', require('./user/routes'))
 
 app.get('/', (req, res) => {
   res.send('Hello World from Kioka')
---
+++ user/routes.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const user = require('./model')
+
+const router = express.Router()
+router.route('/')
+  .get((req, res) => {
+    user.find().then(users => {
+      res.send(users)
+    })
+  })
+  .post((req, res) => {
+    console.log('data from http post', req.body)
+    user.create({
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      dateOfBirth: new Date(req.body.dob).getTime()
+    }).then(user => {
+      res.send(`user ${user.firstname} created with id: ${user._id}`)
+    })
+  })
+
+  router.route('/:id')
+    .get((req, res) => {
+      user.findById(req.params.id).then(user => {
+        res.send(user)
+      })
+    })
+module.exports = router
